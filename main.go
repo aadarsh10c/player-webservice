b225f3272f29994bac9304c06d85944fccb6e632
aadarsh010c <adarsh010c@gmail.com>
2024-02-05 21:48:04 +0530

    partial get service done

--- main.go
+++ main.go
@@ -7,7 +7,14 @@ import (
 	"github.com/aadarsh10c/player-webservice/server"
 )
 
+type InMemoryPlayerStore struct{}
+
+func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	return 123
+}
+
 func main() {
-	server := &server.PlayerServer{}
+	stubStore := &InMemoryPlayerStore{}
+	server := &server.PlayerServer{Store: stubStore}
 	log.Fatal(http.ListenAndServe(":5000", server))
 }
--- server/server.go
+++ server/server.go
@@ -10,10 +10,10 @@ type PlayerStore interface {
 	GetPlayerScore(string) int
 }
 type PlayerServer struct {
-	store PlayerStore
+	Store PlayerStore
 }
 
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	player := strings.TrimPrefix(r.URL.Path, "/players/")
-	fmt.Fprint(w, p.store.GetPlayerScore(player))
+	fmt.Fprint(w, p.Store.GetPlayerScore(player))
 }
